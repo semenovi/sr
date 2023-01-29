96f79e44e52ca90e54b4cf1fa775b17177acc166
madbearing <t79601712352@gmail.com>
2023-01-29 10:38:03 +0300

    fixed, now time in ticks

---
+++ hpc.cs
@@ -0,0 +1,41 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using System.ComponentModel;
+using System.Threading;
+using System.Diagnostics;
+
+
+namespace srntr
+{
+    internal class hpc
+    {
+        private static DateTime _startTime;
+        private static Stopwatch _stopWatch = null;
+        private static TimeSpan _maxIdle =
+            TimeSpan.FromSeconds(10);
+
+        public static DateTime UtcNow
+        {
+            get
+            {
+                if ((_stopWatch == null) ||
+                    (_startTime.Add(_maxIdle) < DateTime.UtcNow))
+                {
+                    Reset();
+                }
+                return _startTime.AddTicks(_stopWatch.Elapsed.Ticks);
+            }
+        }
+
+        private static void Reset()
+        {
+            _startTime = DateTime.UtcNow;
+            _stopWatch = Stopwatch.StartNew();
+        }
+    }
+}
